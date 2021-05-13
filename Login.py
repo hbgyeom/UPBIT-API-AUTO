181edb58f78b16705c40e2ac371b727118ed4f0a
hbgyeom <hbgyeom@khu.ac.kr>
2021-05-13 23:28:14 +0900

    Add Login.py

---
+++ Login.py
@@ -0,0 +1,14 @@
+# Access Key, Secret Key 불러오기, 업비트 로그인
+# Login.py와 같은 디렉토리에 Access Key, Secret Key를 Key.txt로 저장
+
+import pyupbit
+
+# Access Key, Secret Key 불러오기
+f = open("Key.txt", 'r')
+lines = f.readlines()
+access = lines[0].strip()
+secret = lines[1].strip()
+f.close()
+
+# 업비트 로그인
+upbit = pyupbit.Upbit(access, secret)
